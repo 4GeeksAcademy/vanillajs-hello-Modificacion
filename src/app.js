55aa3b4ef76e5152a1add660f46dce4824a1fcfe
Roberto Agredo <memoriaderam@gmail.com>
2024-12-22 03:01:08 -0400

    Update app.js
    update file app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  let numRandon = Math.floor(Math.random());
+
+  let Who = numRandon * who.length;
+  let Action = numRandon * who.length;
+  let What = numRandon * who.length;
+  let When = numRandon * who.length;
+
   console.log("Hello Rigo from the console!");
+  let printer =
+    who[Who] + " " + action[Action] + " " + what[What] + " " + when[When];
+
+  document.querySelector("#excuse").innerHTML = printer;
 };
